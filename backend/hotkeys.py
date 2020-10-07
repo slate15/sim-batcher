822f98847f99984f92f715a9d7fbe59be81c8b07
Slate <slate.b715@gmail.com>
2020-10-06 20:02:33 -0400

    Add ISFL team functionality
    Specifies dictionaries for ISFL teams so that the AHK scripts can navigate
    to the needed pages.

--- backend/hotkeys.py
+++ backend/hotkeys.py
@@ -10,6 +10,7 @@ from settings import *
 
 # Global variables
 # DSFL
+DSFL_TEAMS = ("DAL", "KCC", "LDN", "MIN", "MBB", "NOR", "POR", "TIJ")
 
 # Team list in exhibition game window
 DSFL_TEAM_LIST = {
@@ -33,6 +34,43 @@ DSFL_TEAM_NAV = {
 	"LDN": [2, 1],
 	"MIN": [2, 2],
 	"POR": [2, 3],
+}
+
+# ISFL
+ISFL_TEAMS = ("ARI", "AUS", "BAL", "BER", "CHI", "COL", "HON", "NOL", "NYS", "OCO", "PHI", "SJS", "SAR", "YKW")
+
+ISFL_TEAM_LIST = {
+	"ARI": 0,
+	"AUS": 1,
+	"BAL": 2,
+	"BER": 3,
+	"CHI": 4,
+	"COL": 5,
+	"HON": 6,
+	"NOL": 7,
+	"NYS": 8,
+	"OCO": 9,
+	"PHI": 10,
+	"SJS": 11,
+	"SAR": 12,
+	"YKW": 13
+}
+
+ISFL_TEAM_NAV = {
+	"BAL": [1,0],
+	"BER": [1,1],
+	"CHI": [1,2],
+	"COL": [1,3],
+	"PHI": [1,4],
+	"SAR": [1,5],
+	"YKW": [1,6],
+	"ARI": [2,0],
+	"AUS": [2,1],
+	"HON": [2,2],
+	"NOR": [2,3],
+	"NYS": [2,4],
+	"OCO": [2,5],
+	"SJS": [2,6],
 }
 
 
@@ -100,20 +138,31 @@ def closeGameWindow():
 # selects the "Sim Game" button
 # In essence it takes you from home screen to ready to sim a game
 def createExhibitionSetupScript(home_team_code, away_team_code):
+	home_down_presses = -1
+	away_down_presses = -1
+	if home_team_code in DSFL_TEAMS and away_team_code in DSFL_TEAMS:
+		home_down_presses = DSFL_TEAM_LIST[home_team_code]
+		away_down_presses = DSFL_TEAM_LIST[away_team_code]
+	elif home_team_code in ISFL_TEAMS and away_team_code in ISFL_TEAMS:
+		home_down_presses = ISFL_TEAM_LIST[home_team_code]
+		away_down_presses = ISFL_TEAM_LIST[away_team_code]
+	else:
+		raise ValueError("Both home and away team codes need to be valid for DSFL or ISFL together.")
+
 	script = ""
 	script += "Send !t\n" # Access Tasks menu
 	script += "Send {Down 4}\n" # Navigate to Sim Exhibition Game
 	script += "Send {Enter}\n" # Select Sim Exhibition Game
 	script += "Send {Tab}\n" # Select home team
 
-	if DSFL_TEAM_LIST[home_team_code] > 0:
-		script += "Send {Down " + str(DSFL_TEAM_LIST[home_team_code]) + "}\n" # Navigate to home team within dropdown menu
+	if home_down_presses > 0:
+		script += "Send {Down " + str(home_down_presses) + "}\n" # Navigate to home team within dropdown menu
 
 	script += "Send {Tab}\n" # Select away team
 	script += "Send {Up}\n" # Navigate to top of away team dropdown menu
 
-	if DSFL_TEAM_LIST[away_team_code] > 0:
-		script += "Send {Down " + str(DSFL_TEAM_LIST[away_team_code]) + "}\n" # Navigate to away team within dropdown menu
+	if away_down_presses > 0:
+		script += "Send {Down " + str(away_down_presses) + "}\n" # Navigate to away team within dropdown menu
 
 	script += "Send {Tab 2}" # Select "Sim Game"
 
@@ -123,15 +172,23 @@ def createExhibitionSetupScript(home_team_code, away_team_code):
 # Create an autohotkey script that starts at the DDSPF main menu with league loaded
 # and navigates to a team's home page from the "Teams" menu dropdown
 def createNavScript(team_code):
+	down_presses = [-1,-1]
+	if team_code in DSFL_TEAMS:
+		down_presses = DSFL_TEAM_NAV[team_code]
+	elif team_code in ISFL_TEAMS:
+		down_presses = ISFL_TEAM_NAV[team_code]
+	else:
+		raise ValueError("Team code specified was not a valid DSFL/ISFL team code.")
+
 	script = ""
 	script += "Send {LAlt down}\n" # Alt needs to be held down...
 	script += "Send {t 2}\n" # so that pressing "t" twice navigates to Teams dropdown menu...
 	script += "Send {LAlt up}\n" # and then can be released
 
-	script += "Send {Down " + str(DSFL_TEAM_NAV[team_code][0]) + "}\n" # Press down enough times to access the team's division
+	script += "Send {Down " + str(down_presses[0]) + "}\n" # Press down enough times to access the team's division
 	script += "Send {Right}\n" # Press right to access the teams in that division
-	if DSFL_TEAM_NAV[team_code][1] > 0:
-		script += "Send {Down " + str(DSFL_TEAM_NAV[team_code][1]) + "}\n" # Press down to navigate to team within the division
+	if down_presses[1] > 0:
+		script += "Send {Down " + str(down_presses[1]) + "}\n" # Press down to navigate to team within the division
 	script += "Send {Enter}" # Select the team
 
 	return script
